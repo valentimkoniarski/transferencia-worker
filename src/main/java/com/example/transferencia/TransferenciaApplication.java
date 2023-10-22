469fce7510eb9c90436a88ac955dd204ca8f35c5
valentim.koniarski <valentim.koniarski@hotmail.com>
2023-10-21 23:53:12 -0300

    ajuste depedencias

--- src/main/java/com/example/transferencia/TransferenciaApplication.java
+++ src/main/java/com/example/transferencia/TransferenciaApplication.java
@@ -1,14 +1,17 @@
 package com.example.transferencia;
 
+import dev.valentim.transferencia.Transferencia;
+import dev.valentim.transferencia.repository.TransferenciaRepository;
+import dev.valentim.usuario.Usuario;
+import dev.valentim.usuario.UsuarioRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"dev.valentim.transferencia", "dev.valentim.key"})
-@EnableJpaRepositories(basePackages = {"dev.valentim.transferencia", "dev.valentim.key"})
+@EntityScan(basePackageClasses = {Transferencia.class, Usuario.class})
+@EnableJpaRepositories(basePackageClasses = {TransferenciaRepository.class, UsuarioRepository.class})
 public class TransferenciaApplication {
 
     public static void main(String[] args) {
